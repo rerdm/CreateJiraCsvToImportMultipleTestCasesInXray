82156f42227ab43ab6aa380cfef1c79da90be51c
rerdmann <rene.erdmann@sogeti.com>
2023-07-04 11:18:35 +0200

    [2023-07-04] Update CreateJiraCsvToImportMultipleTestCasesInXray.py

--- CreateJiraCsvToImportMultipleTestCasesInXray.py
+++ CreateJiraCsvToImportMultipleTestCasesInXray.py
@@ -9,32 +9,37 @@ import pandas as pd
 from openpyxl import load_workbook
 
 
-class CreateJiraCsvToImportMultipleTestCasesInXray:
+class ErstelleEineXrayTestCaseListAusAlmXlsTestCases:
     """
-    This class converts the xlsx files (test cases from ALM) and saves them as csv file in the output folder.
+    Mit dieser Klasse is es möglich ALM Test-Cases (XLSX) files aus dem einem Ordner [input]
+    in ein csv file zu übertragen.
+    Dieses File ist XRAY konform und kann über den Test-Case importer importiert werden.
     """
+
 
     def __init__(self, input_folder, output_folder):
         self.input_folder = input_folder
         self.output_folder = output_folder
 
-    def convert(self):
+
+    def konvertiere_xlsx_test_case_files_in_csv(self):
 
         for filename in os.listdir(self.input_folder):
 
             if filename.endswith(".xlsx"):
                 input_path = os.path.join(input_folder, filename)
-                output_path = os.path.join(output_folder, filename[:-5] + ".csv")
+                self.output_path = os.path.join(output_folder, filename[:-5] + ".csv")
                 df = pd.read_excel(input_path)
-                df.to_csv(output_path, index=False)
+                df.to_csv(self.output_path, index=False)
 
-        print("Converting xlsx files from [input folder] and save in [output folder] as csv - successfully ")
+        print("SUCCESS: Konvertiere Test-Cases aus dem [input] ordner und speichere sie im [output] Ordner im csv "
+              "Format")
 
 
 class XlsxSetTestcaseNumber:
     """
-    This class creates a new csv file --> combined.csv
-    This csv file contains all test cases (the test cases are appended one below the other).
+    Diese Klasse erstellt eine neue csv Datei im Ordner [jira_import] --> combined.csv
+    Diese csv Datei beinhaltet alle all test cases (Die Test-Cases sind hier aneinander gehagen).
     Additionally, the issue ID for the test cases is generated correctly (The issue ID = test case number).
     Issue ID 7 = Test case 7 (Must be present in each step of the test case)
     """
@@ -45,14 +50,16 @@ class XlsxSetTestcaseNumber:
         self.counter_for_files = 1
 
     def process_xlsx_files(self):
+
         for file_name in self.file_list:
+
             file_path = os.path.join('input', file_name)
 
             # Open XLSX-Datei with openpyxl
             wb = load_workbook(file_path)
             sheet = wb.active
 
-            # Will look for the header with the content Step name
+            # Will look for the header with the content Step Name
             header_col_index = None
             for col_idx, cell in enumerate(sheet[1]):
                 if cell.value == "Step Name":
@@ -92,20 +99,22 @@ class ReadXlsxToCSV:
             if filename.endswith('.xlsx'):
                 file_path = os.path.join(self.csv_folder, filename)
                 time.sleep(0.2)
-                print(" - XLSX file found :   ", filename)
+                print(" - XLSX Test-Cases aus dem Ordner [input] :   ", filename)
 
                 wb = load_workbook(file_path)
                 sheet = wb.active
 
-                # Step Name | Summary | Test Type | Expected Result
+                # Test Case Identifier | Summary | Action | Expected Result
 
                 sheet.insert_cols(2)
+                #sheet.cell(row=1, column=1).value = "Test Case Identifier"
                 sheet.cell(row=1, column=2).value = "Summary"
                 sheet.cell(row=2, column=2).value = filename
+                sheet.cell(row=1, column=3).value = "Action"
 
-                sheet.insert_cols(3)
-                sheet.cell(row=1, column=3).value = "Test Type"
-                sheet.cell(row=2, column=3).value = "Manual"
+                #sheet.insert_cols(3)
+                #sheet.cell(row=1, column=3).value = "Test Type"
+                #sheet.cell(row=2, column=3).value = "Manual"
 
                 for i, row in enumerate(sheet.iter_rows(values_only=True)):
                     modified_row = list(row)
@@ -114,13 +123,15 @@ class ReadXlsxToCSV:
             wb.save(self.output_csv_name)
             wb.close()
 
+        wb.save(self.output_csv_name)
+
         output_file = os.path.join(self.output_folder, self.output_csv_name)
 
-        with open(output_file, 'w', newline='') as csvfile:
+        with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
             writer = csv.writer(csvfile, delimiter=';')
             writer.writerows(self.all_rows)
 
-        print('The combined CSV file has been created: {}' + output_file)
+        print('SUCCESS: Die Datei [combined.csv] wurde im Ordner [jira_import] erstellt ')
 
 
 class CSVReader:
@@ -140,21 +151,20 @@ class CSVReader:
 
     def read_csv(self):
         i = -1
-        with open(self.filepath, 'r') as csvfile:
+        with open(self.filepath, 'r', encoding='utf-8') as csvfile:
             reader = csv.reader(csvfile, delimiter=";")
             for row_num, row in enumerate(reader, start=1):
                 if row:
                     i = i + 1
 
                 if row[0] == "Step Name":
-                    print(f' - Column A Step name found in row [{i}] of the CSV-File.')
                     self.lines_to_delete.append(i)
 
-        print("Found the header in fallowing lines :", self.lines_to_delete)
         return self.lines_to_delete
 
 
 class SubtractListElements:
+
     """
     This class is passed a list.
     This is manipulated so that it can be used to remove the corresponding lines from the csv.
@@ -176,22 +186,18 @@ class SubtractListElements:
             subtracted_value = self.value_list[i] - i
             result_list.append(subtracted_value)
 
-        print("Manipulated List with the proper header numbers to delete : ", result_list)
         return result_list
 
 
 class CSVDeleteLines:
-    """
-    In this class the csv file is passed to remove the corresponding rows.
-    The class creates a new csv whose columns are JIRA compliant.
-    """
+
 
     def __init__(self, file_path):
         self.file_path = file_path
         self.rows = []
 
     def read_csv(self):
-        with open(self.file_path, 'r', newline='') as csvfile:
+        with open(self.file_path, 'r', newline='', encoding='utf-8') as csvfile:
             reader = csv.reader(csvfile, delimiter=";")
             self.rows = list(reader)
 
@@ -200,53 +206,42 @@ class CSVDeleteLines:
             if row_index < len(self.rows):
                 del self.rows[row_index]
 
+        time.sleep(0.5)
+        print("SUCCESS: Die Zeilen wurden erfolgreich aus der [combined.csv] gelöscht : ", rows_to_delete)
+        print(" - Zeilen gelöscht um nur einen Unique Header in der Datei zu haben ( Zeile 1 )\n"
+              " - Dies ist notwendig um ein Xray konformes csv file zu erzeugen.\n"
+              " - Erstelltes file im ordner [output]:  [multiple_testcases_file_for_jira.csv]")
+
     def save_csv(self, output_file_path):
 
         try:
-            with open(output_file_path, 'w', newline='') as csvfile:
+            with open(output_file_path, 'w', newline='', encoding='utf-8') as csvfile:
                 writer = csv.writer(csvfile, delimiter=";")
                 writer.writerows(self.rows)
 
         except:
-            sys.exit("ERROR: The file multiple_testcases_file_for_jira.csv is already open.\n"
-                     "Close the file and run the programm again.\n"
-                     "NOTE: The file will be overwritten - to prevent overriding store the file in a other location.\n"
+            sys.exit("ERROR: Die Datei [multiple_testcases_file_for_jira.csv] ist offen.\n"
+                     "Schließen sie die Datei und starten sie das Programm erneut.\n"
+                     "HINWEIS: Die Datei wird überschrieben in diesem Ordner wird überschrieben \n"
+                     ", bitte sichern sie die Datei vorher.\n"
                      )
-        print("Headlines deleted to have only one headline in the file multiple_testcases_file_for_jira.csv")
-
-
-class ChangeCsvCodingFromAsiToUTF8:
-    """
-    In this class the csv file is passed, which was adapted for JIRa.
-    In order for all umlauts to be displayed correctly in JIRA, the file must conform to the UTF-8 standard.
-    The class changes the encoding of the file so that a JIRA import does not show <?> in the text.
-    """
-
-    def __init__(self, csv_name):
-        self.csv_name = csv_name
-
-    def convert_encoding(self):
-        # Actual coding
-        aktuelle_codierung = 'cp1252'  # Example: ANSI (Windows-1252)
-
-        with codecs.open(self.csv_name, 'r', encoding=aktuelle_codierung) as datei:
-            inhalt = datei.read()
-
-        # Open file and save it as UTF-8-coded file
-        with codecs.open(self.csv_name, 'w', encoding='utf-8') as datei:
-            datei.write(inhalt)
-
-        print(f'File: {csv_name} was saved in UTF-8 format')
-
+        time.sleep(0.5)
+        print("SUCCESS: File erstellt  im ordner [jira_import]:  multiple_testcases_file_for_jira.csv"
+              " - Mit diesem File können nun mehrere Test-Cases mit einem mal über den Xray-Test-Case-Importer"
+              "importiert werden.\n")
 
 if __name__ == '__main__':
 
     folders = os.listdir()
+
     if "output" in folders:
         pass
+
     else:
-        sys.exit("ERROR: The [output] folder does not exist in the current directory.\n"
-                 "Please create a folder in the current directory and start the program again.\n")
+        sys.exit("ERROR: Es existiert kein Ordner [output] im aktuellen Verzeichnis.\n"
+                 "Erstellen sie einen Ordner [output].\n"
+                 "In diesem Ordner werden die Test-Cases als csv datei zwischen gespeichert\n"
+             )
 
     count_xlsx_files = 0
 
@@ -255,26 +250,33 @@ if __name__ == '__main__':
         files_in_input_folder = os.listdir('input/')
 
         for file in files_in_input_folder:
+            if not file.endswith(".xlsx"):
+                sys.exit(" ERROR: Der Ordner [input] darf keinen Unter-Ordner haben.\n "
+                         "Bitte löschen sie bzw.verschieben sie den/die Ordner aus dem Input Ordner.\n")
+
+        for file in files_in_input_folder:
+
             if file.endswith(".xlsx"):
                 count_xlsx_files += 1
-        if count_xlsx_files > 0:
+        if count_xlsx_files > 2:
             pass
         else:
-            sys.exit("ERROR: Der Ordner [input] muss mindestens ein XLSX-File beinhalten (Testfall).\n "
+            sys.exit(" ERROR: Der Ordner [input] muss mindestens 2 XLSX-File beinhalten (Testfälle).\n "
                      "Bitte fügen sie mindestens einen testfall in das Verzeichnis.\n")
 
     else:
-        sys.exit("ERROR: The [input] folder must contain at least one XLSX file (test case).\n"
-                 "Please create a folder in the current directory and start the program again.\n"
-                 "NOTE: The folder must contain at least one XLSX-FIle (test case).\n")
+        sys.exit("ERROR: Es existiert kein Ordner [input] im aktuellen Verzeichnis.\n"
+                 "Erstellen sie einen Ordner [input], in dem sie die Test-Cases als XLSX ablegen (Mindestens 2!!).\n")
 
     if "jira_import" in folders:
         pass
     else:
-        sys.exit("ERROR: The folder [jira_import] does not exist in the current directory.\n"
-                 "Please create a folder in the current directory and start the program again.\n"
-                 "NOTE: For safety, this folder should be empty to prevent old results\n"
-                 "will be overridden. \n")
+        sys.exit("ERROR: Es existiert kein Ordner [jira_import] im aktuellen Verzeichnis.\n"
+                 "Erstellen sie einen Ordner [jira_import].\n"
+                 "In diesem Ordner wird das file [multiple_testcases_file_for_jira] erstellt "
+                 "das alle Testfälle beinhaltet.\n"
+                 "Das erstellte File ist XRAY kompatible und kann über den Xray-Test-Case-Importer importiert werden.\n"
+                 )
 
     output_folder = "output"
     input_folder = "input"
@@ -284,8 +286,12 @@ if __name__ == '__main__':
     current_dir = os.getcwd()
     input_folder = os.path.join(current_dir, input_folder)
 
-    converter = CreateJiraCsvToImportMultipleTestCasesInXray(input_folder, output_folder)
-    converter.convert()
+    converter = ErstelleEineXrayTestCaseListAusAlmXlsTestCases(
+        input_folder=input_folder,
+        output_folder=output_folder
+    )
+    
+    converter.konvertiere_xlsx_test_case_files_in_csv()
 
     xlsx_files = os.listdir('input')
     set_test_case_number_to_csv = XlsxSetTestcaseNumber(xlsx_files)
@@ -323,7 +329,3 @@ if __name__ == '__main__':
     csv_deleter.save_csv('jira_import/multiple_testcases_file_for_jira.csv')
 
     time.sleep(2)
-
-    csv_name = "jira_import/multiple_testcases_file_for_jira.csv"
-    csv_converter = ChangeCsvCodingFromAsiToUTF8(csv_name)
-    csv_converter.convert_encoding()
